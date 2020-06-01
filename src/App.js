9677b73a0f553b61017b3f03e838337064c0976b
michelleRahman21 <michgomez1807@hotmail.com>
2020-06-01 13:15:55 -0400

    created home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Homepage from './homepage.js'
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Homepage />
     </div>
   );
 }
